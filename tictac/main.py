b35334fd10f1c0d8901acc0c1011076bcee6586a
Scott Stafford <scott.stafford@databricks.com>
2020-09-28 15:03:12 -0400

    Add user vs random and print boards

--- tictac/main.py
+++ tictac/main.py
@@ -1,14 +1,16 @@
-from tictac.board import Board, play_game, play_random_move
+from tictac.board import play_game, play_random_move
 
 
 def play_user_move(board):
-    spot = input("Pick a spot: ")
-    print("Spot: " + spot)
-    return board.play_move(spot)
+    board.print_board()
+    move = input("Pick a spot: ")
+    new_board = board.play_move(int(move))
+    new_board.print_board()
+    return new_board
 
 
 def play():
-    game_board = play_game(play_random_move, play_random_move)
+    game_board = play_game(play_user_move, play_random_move)
     game_board.print_board()
 
 
